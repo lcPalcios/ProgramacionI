8a78c9a5715d2757c81a10f06e2dcdcf343e8c52
lcPalcios <dalex_31@hotmail.com>
2015-10-28 06:20:38 -0600

    Clase 27-10

---
+++ Alarma/ConsoleApplication1/Module1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Console
+Imports System.Media
+Module Module1
+
+
+    Sub Main()
+        Dim HoraFin As DateTime
+        Dim diferencia As TimeSpan
+        HoraFin = Now.AddSeconds(5)
+        HoraFin = Now.AddMinutes(1)
+
+        Console.CursorVisible = False
+
+
+        While Now <= HoraFin
+            Console.SetCursorPosition(1, 1)
+            diferencia = HoraFin - Now
+            WriteLine("Faltan {0} minutos y {1} segundos", diferencia.Minutes, diferencia.Seconds)
+        End While
+
+        WriteLine("Hora de levantarse!")
+
+        Dim player As New SoundPlayer("C:\Users\LC\Documents\GitHub\Clases\Programacion II\ProgramacionI\Alarma\ConsoleApplication1\sound\gallo2.wav")
+
+        player.Play()
+
+    End Sub
+
+End Module
